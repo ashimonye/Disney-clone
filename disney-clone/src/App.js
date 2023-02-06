a64a2ecea115f5df32273f884c37f55f7bca8f52
Gideon Nwankwo <uberposeidonn@gmail.com>
2023-02-06 04:49:40 +0100

    Building Landing Login Page

--- disney-clone/src/App.js
+++ disney-clone/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {Routes,
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Login from './components/Login'
+import Header from './components/Header'
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      Hi
+      <Router>
+        <Header />
+        <Routes>
+        <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ disney-clone/src/components/Header.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+
+const Header = (props) => {
+    return (
+        <div>
+            Header
+        </div>
+    );
+};
+
+export default Header;
