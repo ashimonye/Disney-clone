3c1a8d83387a5659e0be08fa885c8561349a459b
Gideon Nwankwo <uberposeidonn@gmail.com>
2023-02-09 23:34:37 +0100

    Update Header.js

--- disney-clone/src/components/Header.js
+++ disney-clone/src/components/Header.js
@@ -12,6 +12,32 @@ const Header = (props) => {
           <Logo>
             <img src="/images/logo.svg" alt="Disney+" />
           </Logo>
+          <NavMenu>
+          <a href="/home">
+              <img src="/images/home-icon.svg" alt="HOME" />
+              <span>HOME</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" alt="SEARCH" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" alt="ORIGINALS" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" alt="MOVIES" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" alt="SERIES" />
+              <span>SERIES</span>
+            </a>
+          </NavMenu>
         </Nav>
       );
     };
