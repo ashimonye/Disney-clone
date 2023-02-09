94bd4919ed17b364830eaa9fb3f819247bab3d77
Gideon Nwankwo <uberposeidonn@gmail.com>
2023-02-09 13:34:03 +0100

    c

---
+++ disney-clone/src/firebase.js
@@ -0,0 +1,33 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCrDbICnUE9D_4PKr2P63ifY4Ts-B9BbrA",
+//   authDomain: "aluusi-disney-9db5d.firebaseapp.com",
+//   projectId: "aluusi-disney-9db5d",
+//   storageBucket: "aluusi-disney-9db5d.appspot.com",
+//   messagingSenderId: "27243960567",
+//   appId: "1:27243960567:web:eede11ea69976961d65bf2",
+//   measurementId: "G-6Q6MGBZMBC"
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// const storage = firebase.storage();
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export { auth, provider, storage };
+// export default db;
